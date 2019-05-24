2d8311581d5354282576c9fdf115a2e8391480ac
cristianpb <felipebrokate@gmail.com>
2019-05-24 10:33:49 +0200

    Remove basenmae from url

--- src/app/predictions.service.ts
+++ src/app/predictions.service.ts
@@ -16,25 +16,21 @@ export class PredictionsService {
 
   constructor(private http: HttpClient) { }
 
-  testPrediction() {
-    return this.http.get(`${environment.api}`);
-  }
-
   objectDection(formData: FormData | {}) {
-    return this.http.post(`${environment.api}/object_detection`, formData);
+    return this.http.post(`/api/object_detection`, formData);
   }
 
   classPrediction(formData: FormData) {
-    return this.http.post<PredBox[]>(`${environment.api}/predict`, formData);
+    return this.http.post<PredBox[]>(`/api/predict`, formData);
   }
 
   callSivnorm(marque, modele, tbRefname) {
-    return this.http.get<SivnormResponse>(`${environment.sivnorm}/norm/${tbRefname}?marque=${marque}&modele=${modele}`);
+    return this.http.get<SivnormResponse>(`/norm/${tbRefname}?marque=${marque}&modele=${modele}`);
   }
 
   callSivnormCsv(files, tbRefname) {
     const formData: FormData = new FormData();
     formData.append('file', files[0], files[0].name);
-    return this.http.post(`${environment.sivnorm}/norm/${tbRefname}`, formData, {responseType: 'text'});
+    return this.http.post(`/norm/${tbRefname}`, formData, {responseType: 'text'});
   }
 }
