6815a47f3fc2b5e9735e72c32a2bfb795b517d46
cristianpb <felipebrokate@gmail.com>
2019-12-10 13:33:38 +0100

    Use set env file to inject environment variables

---
+++ set-env.ts
@@ -0,0 +1,26 @@
+//import { writeFile } from 'fs'; 
+
+const fs = require('fs');
+
+// Configure Angular `environment.ts` file path
+const targetPath = './src/environments/environment.prod.ts';
+
+// Load node modules
+const colors = require('colors');
+const urlSivnorm = process.env.URL_SIVNORM || null ;
+const urlMatchvec = process.env.URL_MATCHVEC || null ;
+
+const envConfigFile = `export const environment = {
+  production: true,
+  apiMatchvec: '${urlMatchvec}',
+  apiSivnorm: '${urlSivnorm}'
+};
+`;
+
+fs.writeFile(targetPath, envConfigFile, function (err) {
+   if (err) {
+       throw console.error(err);
+   } else {
+       console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+   }
+});
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
-const urlSivnorm = process.env.URL_SIVNORM || null ;
-const urlMatchvec = process.env.URL_MATCHVEC || null ;
-
 export const environment = {
   production: true,
-  apiMatchvec: urlMatchvec,
-  apiSivnorm: urlSivnorm
+  apiMatchvec: null,
+  apiSivnorm: null
 };
